0f11e688bfe4d324e060a303b8d582cf4b33d63d
SimulEOC <56169926+SimulEOC@users.noreply.github.com>
2019-10-07 12:39:20 -0500

    Magic test #1
    Currently within the CharacterFirstPerson scene, within SampleScenes
    left click to fire, right click spawns target

---
+++ 1p test 2/Assets/GunController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    public GameObject gunModel;
+    public GameObject gunTarget;
+    public float speed;
+    public GameObject GunStartPosition;
+    public GameObject GunEndPosition;
+    public Rigidbody rb;
+    public GameObject bullet;
+    public GameObject target;
+    public GameObject targetSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 100.0f;
+        gunTarget.transform.position = gunModel.transform.position;
+        Vector3 offset = new Vector3(0.0f, 0.0f, 0.5f);
+        GunEndPosition.transform.position = gunModel.transform.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gunModel.transform.position = Vector3.Lerp(gunModel.transform.position, gunTarget.transform.position, 0.1f * Time.deltaTime * speed);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(FireWeapon());
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Instantiate(target, targetSpawn.transform.position, targetSpawn.transform.rotation);
+        }
+
+        IEnumerator FireWeapon()
+        {
+            // set target to end position
+            gunTarget.transform.position = GunEndPosition.transform.position;
+            // wait 0.2 sec
+            yield return new WaitForSeconds(0.2f);
+            // set target back to start position
+            gunTarget.transform.position = GunStartPosition.transform.position;
+
+            Instantiate(bullet, GunStartPosition.transform.position, GunStartPosition.transform.rotation);
+        }
+
+    }
+}
