e1ec48d1b231bc27b32b599d47ad43d9e1d34249
SimulEOC <56169926+SimulEOC@users.noreply.github.com>
2019-10-08 13:58:03 -0500

    Health change
    This stops health from being higher than max for targets

--- 1p test 2/Assets/TargetController.cs
+++ 1p test 2/Assets/TargetController.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class TargetController : MonoBehaviour
 {
     public GameObject bullet;
+    public float maxHealth = 100.0f;
     public float health = 100.0f;
     public GameObject target;
 
     public void Update()
     {
-        if (health <= 100)
+        if (health <= 0)
         {
             Object.Destroy(target);
         }
+        
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
     }
+    
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("bullet"))
