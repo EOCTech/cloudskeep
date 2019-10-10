c0f48d58baf7c398e1a98ea058fe918ac04abb33
WEBPM <contact@eoctech.edu>
2019-10-10 13:48:21 -0500

    2.8.f1 update

--- 1p test 2/Assets/WeaponVariables.cs
+++ 1p test 2/Assets/WeaponVariables.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class WeaponVariables : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // sword
+    public static bool hasSword = false;
+    public static float swordDamage = 25.0f;
+    public static float swordSpeed = 10.0f;
+    // mace
+    public static bool hasMace = false;
+    public static float maceDamage = 35.0f;
+    public static float maceSpeed = 8.0f;
+    // battleaxe
+    public static bool hasBattleaxe = false;
+    public static float battleaxeDamage = 50.0f;
+    public static float battleaxeSpeed = 4.0f;
 }
